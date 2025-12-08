e231589c21b070ede432b7735d1b65ad0d9d4537
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-12-09 04:00:24 +0600

    done

---
+++ src/components/chat/menu/HeaderManu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderManu = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default HeaderManu;
--- src/components/chat/menu/InputBar.jsx
+++ src/components/chat/menu/InputBar.jsx
@@ -1,15 +1,55 @@
-import { Box, InputBase } from '@mui/material';
+import { Box, InputBase ,styled} from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
+
+
+
+const Component=styled(Box)`
+background:#fff;
+height:45px;
+border-bottom:1px solid #f2f2f2;
+display:flex;
+align-items:center;
+
+`;
+
+const Wrapper=styled(Box)`
+background-color:#f0f2f5;
+position:relative;
+margin:0 13px;
+width:100%;
+border-radius:10px;
+`;
+
+
+const Icon=styled(Box)`
+position:absolute;
+height:100%;
+padding:6px 10px;
+color:#919191;
+`;
+
+const InputField=styled(InputBase)`
+width:100%;
+padding:16px;
+height:15px;
+padding-left:65px;
+font-size:14px;
+
+`;
+
+
+
+
 
 
 const InputBar = () => {
     return (
-        <Box>
-            <Box>
-            <Box>
-            <SearchIcon/>
-           </Box>
-          <InputBase
+        <Component>
+            <Wrapper>
+            <Icon>
+            <SearchIcon  fontSize='small'/>
+           </Icon>
+          <InputField
             placeholder='Search or start new chat'
             inputProps={{'aria-label':'search'}}
             />
@@ -20,8 +60,8 @@ const InputBar = () => {
 
 
 
-        </Box>
-        </Box>
+        </Wrapper>
+        </Component>
     );
 };
 
