97399e5679f166276000ae51178918dd117dd4e5
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-12-10 03:00:23 +0600

    backend part start

---
+++ chatapp-backend-part/routes/api.js
@@ -0,0 +1,27 @@
+import express from "express";
+const router = express.Router();
+
+
+import * as UsersController from "../app/controllers/UsersController.js";
+import AuthMiddleware from "../app/middlewares/AuthMiddleware.js";
+
+
+// Users
+router.post("/Registration", UsersController.Registration)
+router.post("/Login", UsersController.Login)
+router.get("/ProfileDetails",AuthMiddleware,UsersController.ProfileDetails)
+router.post("/ProfileUpdate",AuthMiddleware, UsersController.ProfileUpdate)
+router.get("/EmailVerify/:email", UsersController.EmailVerify)
+router.post("/CodeVerify", UsersController.CodeVerify)
+router.post("/ResetPassword", UsersController.ResetPassword)
+
+
+
+
+
+
+
+
+export default router;
+
+
